cdb5174b8f877d35184bf2832064139526d4088c
Michael Ruf <michael.ruf@gmail.com>
2023-09-12 19:28:26 +0200

    Initial project setup

---
+++ src/main/java/de/jtechsoft/playlister/playlister/PlaylisterApplication.java
@@ -0,0 +1,13 @@
+package de.jtechsoft.playlister.playlister;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlaylisterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlaylisterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/jtechsoft/playlister/playlister/PlaylisterApplicationTests.java
@@ -0,0 +1,13 @@
+package de.jtechsoft.playlister.playlister;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlaylisterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
